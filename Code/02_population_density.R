c63d759bdb709f198b582168453d73f29a42bce9
jonahmende <jonahme@icloud.com>
2025-10-09 17:43:30 +0200

    Create 02_population_density.R

---
+++ Code/02_population_density.R
@@ -0,0 +1,42 @@
+# Code for estimating population densities
+
+# A package is needed for point pattern analysis
+# install.packages("spatstat")
+library(spatstat)
+
+# let's use the bei data:
+# data description:
+# https://CRAN.R-project.org/package=spatstat
+
+bei
+
+# plotting the data
+plot(bei)
+
+# changing dimension - cex
+plot(bei, cex=.2)
+
+# changing the symbol - pch
+plot(bei, cex=.2, pch=19)
+
+# additional datasets
+bei.extra
+plot(bei.extra)
+
+# let's use only part of the dataset: elev
+plot(bei.extra$elev)
+elevation <- bei.extra$elev
+plot(elevation)
+
+# second method to select elements
+elevation2 <- bei.extra[[1]]        # double brackets because we deal with raster data
+plot(elevation2)
+
+# passing from points to a countinuous surface
+densitymap <- density(bei)
+plot(densitymap)
+points(bei, cex=.2)
+
+########## DAY 2
+
+
