44f4a560f7a083bbcde39a0643299c21b12c5476
jonahmende <jonahme@icloud.com>
2025-10-17 10:48:05 +0200

    Update 03_population_dispersion.R

--- Code/03_population_dispersion.R
+++ Code/03_population_dispersion.R
@@ -1,8 +1,8 @@
 # install.packages("sdm")
 # install.packages("terra")
 
-library(sdm)
 library(terra)
+library(sdm)
 
 file <- system.file("external/species.shp", package="sdm")
 # [1] "/usr/local/lib/R/site-library/sdm/external/species.shp"
@@ -12,6 +12,16 @@ rana
 
 rana$Occurrence
 
+# how to acces single coordinate data???
+
+
 plot(rana)
 
-# how to acces single coordinate data???
+# select all presences and absences
+pres <- rana[rana$Occurrence==1]
+abse <- rana[rana$Occurrence==0]  # or !=1
+
+# plot the presences and absences with different colors
+par(mfrow=c(1,2))
+plot(pres, col='blue')
+plot(abse, col='orange')
